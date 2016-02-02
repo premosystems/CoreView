ba9544297c2187653a87302fc45d8a0a416809fa
Vincil Bishop <vincil.bishop@vbishop.com>
2016-02-02 10:09:01 -0600

    Updated for concrete view naming

--- Pod/Classes/CVConcreteViews.h
+++ Pod/Classes/CVConcreteViews.h
@@ -11,27 +11,31 @@
 #import "CVSingluarModelView.h"
 #import "CVPluralModelView.h"
 
+/*
+ A collection of concrete base classes that an app can inherit from.
+ */
+
 // SINGULAR INTERFACES //
 
-CV_SINGULAR_VIEW_INTERFACE(CVSingularModelTableViewController, UITableViewController)
+CV_SINGULAR_VIEW_INTERFACE(CVSingularTableViewController, UITableViewController)
 
-CV_SINGULAR_VIEW_INTERFACE(CVSingularModelCollectionViewController, UICollectionViewController)
+CV_SINGULAR_VIEW_INTERFACE(CVSingularCollectionViewController, UICollectionViewController)
 
-CV_SINGULAR_VIEW_INTERFACE(CVSingularModelTableViewCell, UITableViewCell)
+CV_SINGULAR_VIEW_INTERFACE(CVSingularTableViewCell, UITableViewCell)
 
-CV_SINGULAR_VIEW_INTERFACE(CVSingularModelCollectionViewCell, UICollectionViewCell)
+CV_SINGULAR_VIEW_INTERFACE(CVSingularCollectionViewCell, UICollectionViewCell)
 
-CV_SINGULAR_VIEW_INTERFACE(CVSingularModelCollectionReusableView, UICollectionReusableView)
+CV_SINGULAR_VIEW_INTERFACE(CVSingularCollectionReusableView, UICollectionReusableView)
 
-CV_SINGULAR_VIEW_INTERFACE(CVSingularModelView, UIView)
+CV_SINGULAR_VIEW_INTERFACE(CVSingularView, UIView)
 
-CV_SINGULAR_VIEW_INTERFACE(CVSingularModelViewController, UIViewController)
+CV_SINGULAR_VIEW_INTERFACE(CVSingularViewController, UIViewController)
 
 // PLURAL INTERFACES //
 
-CV_PLURAL_VIEW_INTERFACE(CVPluralModelViewController, UIViewController)
+CV_PLURAL_VIEW_INTERFACE(CVPluralViewController, UIViewController)
 
-CV_PLURAL_VIEW_INTERFACE(CVPluralModelTableViewController, UITableViewController)
+CV_PLURAL_VIEW_INTERFACE(CVPluralTableViewController, UITableViewController)
 
-CV_PLURAL_VIEW_INTERFACE(CVPluralModelCollectionViewController, UITableViewController)
+CV_PLURAL_VIEW_INTERFACE(CVPluralCollectionViewController, UITableViewController)
 
